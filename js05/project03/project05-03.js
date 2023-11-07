d4d71558244692ceb910e7edf8bcc7bba00bdef1
fminor5 <fminor5@gmail.com>
2023-11-07 11:00:01 -0600

    The links are created but the fragment under the href is not created. This is on line 31.

--- js05/project03/project05-03.js
+++ js05/project03/project05-03.js
@@ -17,7 +17,21 @@ const heading = "H2";
 for (let n = sourceDoc.firstElementChild; n != null; n = n.nextElementSibling) {
   if (n.nodeName === heading) {
     let anchor = document.createElement("a");
-    //     working on 6.b
-  } else {
+    // 6.b
+    anchor.value = "doclink" + headingCount;
+    // 6.c
+    n.insertBefore(anchor, n.firstElementChild);
+    // 6.d
+    let listItem = document.createElement("li");
+    let link = document.createElement("a");
+    listItem.appendChild(link);
+    // 6.e
+    link.textContent = n.textContent; // check this
+    // 6.f
+    listItem.href = "#doclink" + headingCount; // check this
+    // 6.g
+    toc.appendChild(listItem);
+    // 6.h
+    headingCount++;
   }
 }
