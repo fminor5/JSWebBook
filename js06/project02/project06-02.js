c46e256025b257e616683e9e196dd71a5f133d16
fminor5 <fminor5@gmail.com>
2023-11-30 12:45:15 -0600

    Working on these files.

--- js06/project02/project06-02.js
+++ js06/project02/project06-02.js
@@ -7,3 +7,17 @@
 
       Filename: project06-02.js
 */
+
+document.addEventListener("load", function () {
+  let allSelect = document.querySelectorAll("form#govLinks select"); // 4 needs to be checked
+
+  for (let i = 0; i < allSelect.length; i++) {
+    // 5
+    allSelect[i].onchange = function (evt) {
+      //5a
+      let linkURL = evt.target.value; //5b
+
+      let newWin = window.open(linkURL); // 5c
+    };
+  }
+});
