218da52c4d25b62f8f9f91166622cb8ec2dd809e
fminor5 <fminor5@gmail.com>
2023-11-30 10:08:44 -0600

    Completed files for coding challenge 1.

--- js06/project01/project06-01.js
+++ js06/project01/project06-01.js
@@ -7,3 +7,22 @@
 
       Filename: project06-01.js
 */
+
+// Declaring variables.
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function () {
+  if (pwd.validity.patternMismatch) {
+    pwd.setCustomValidity(
+      "Your password must be at least 8 characters with at least one letter and one number."
+    );
+  } else if (pwd === pwd2) {
+    pwd.setCustomValidity("Your passwords must match");
+  } else {
+    pwd.setCustomValidity("");
+  }
+});
+
+// zapatos1
