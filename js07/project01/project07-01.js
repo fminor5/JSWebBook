8027fcd8dac6825561d5690a645cdf93be99e3a2
fminor5 <fminor5@gmail.com>
2023-12-12 11:28:36 -0600

    Adding coding challenge 1 completed.

--- js07/project01/project07-01.js
+++ js07/project01/project07-01.js
@@ -13,4 +13,25 @@ let signupForm = document.getElementById("signup");
 signupForm.addEventListener("submit", function (e) {
   let pwd = document.getElementById("pwd").value;
   let feedback = document.getElementById("feedback");
+
+  // Preventing default
+  e.preventDefault();
+
+  // Declaring regular expressions variables
+  let regex1 = /[A-Z]/;
+  let regex2 = /[0-9]/;
+  let regex3 = /[!\$#%]/;
+
+  if (pwd.length < 8) {
+    feedback.textContent = "Your password must be at least 8 characters.";
+  } else if (regex1.test(pwd) === false) {
+    feedback.textContent = "Your password must include an uppercase letter.";
+  } else if (regex2.test(pwd) === false) {
+    feedback.textContent = "Your password must include a number.";
+  } else if (regex3.test(pwd) === false) {
+    feedback.textContent =
+      "Your password must include one of the following: !$#%";
+  } else {
+    signupForm.submit();
+  }
 });
