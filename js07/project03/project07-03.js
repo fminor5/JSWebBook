1a7a053895ba028ce90e22ef070475570b47c63b
fminor5 <fminor5@gmail.com>
2023-12-13 12:22:49 -0600

    Adding completed coding challenge 3.

--- js07/project03/project07-03.js
+++ js07/project03/project07-03.js
@@ -13,3 +13,30 @@ let daysLeftBox = document.getElementById("days");
 let hrsLeftBox = document.getElementById("hours");
 let minsLeftBox = document.getElementById("minutes");
 let secsLeftBox = document.getElementById("seconds");
+
+setInterval(countdown, 1000); //3 check this one
+
+// Creating a countdown function (step 4) which will have
+// steps 5-11
+function countdown() {
+  let nowVar = new Date(); //5
+  currentTime.textContent = nowVar.toLocaleString(); //6
+
+  let newYear = new Date(2024, 0, 1); //7
+
+  let nextYear = nowVar.getFullYear() + 1; //8
+
+  newYear.setFullYear(nextYear); //9
+
+  // Doing the calculations for days, hours, minutes, and seconds, step 10.
+
+  let daysLeft = (newYear - nowVar) / (1000 * 60 * 60 * 24);
+  let hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
+  let minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
+  let secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
+
+  daysLeftBox.textContent = Math.floor(daysLeft);
+  hrsLeftBox.textContent = Math.floor(hrsLeft);
+  minsLeftBox.textContent = Math.floor(minsLeft);
+  secsLeftBox.textContent = Math.floor(secsLeft);
+}
