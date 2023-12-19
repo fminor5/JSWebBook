363e230793f73dbc5de4bf483e8d0caf570ba306
fminor5 <fminor5@gmail.com>
2023-12-19 10:42:24 -0600

    Adding files for chapter 8.

---
+++ js08/project01/project08-01.js
@@ -0,0 +1,18 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 8
+      Project 08-01
+
+      Project to create a timer object
+
+      Filename: project08-01.js
+*/
+
+/*--------------- Object Code --------------------*/
+
+/*---------------Interface Code -----------------*/
+
+/* Interface Objects */
+let minBox = document.getElementById("minutesBox");
+let secBox = document.getElementById("secondsBox");
+let runPauseTimer = document.getElementById("runPauseButton");
