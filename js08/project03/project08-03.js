cfe255477e090769909d3f91f43b6579274bbd13
fminor5 <fminor5@gmail.com>
2023-12-29 13:38:26 -0600

    Adding parts of the coding challenge.

--- js08/project03/project08-03.js
+++ js08/project03/project08-03.js
@@ -9,6 +9,46 @@
 */
 
 /*---------------- Object Code ----------------------*/
+//s3 Creating a literal object with and empty array
+let cart = {
+  items: [],
+  addItem: function (foodItem) {
+    this.items.push(foodItem);
+  },
+};
+
+//s4 Creating a constructor function Pizza
+function Pizza(size, crust) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = [];
+}
+
+//s5 Creating a constructor function Topping
+function Topping(name, side) {
+  this.name;
+  this.crust;
+}
+
+//s6 Adding the addToCart() method to the Pizza prototype.
+Pizza.prototype.addToCart = function (cart) {
+  cart.items.push(this);
+};
+
+//s7 Adding the summarize() method to the Pizza prototype.
+Pizza.prototype.summarize = function () {
+  //s7a Adding a variable
+  let summary = "Pizza";
+  //s7b adding the values
+  summary = this.size + " " + this.crust;
+  //s7c Running a for-loop and for each topping we add the name and sice.
+  for (let i = 0; i < this.toppings.length; i++) {
+    summary = this.toppings[i].name;
+    summary = this.toppings[i].side;
+  }
+  //s7d Returning the value
+  return summary;
+};
 
 /*----------------------------- Interface Code -------------------------*/
 
@@ -66,4 +106,18 @@ function buildPizza() {
 }
 
 // Function to add the built pizza to the shopping cart
-function updateCart() {}
+function updateCart() {
+  //s9a Runninng the buildPizza function and storing it in a variable
+  let myPizza = buildPizza();
+  //s9b Applying the addItem to the cart object
+  cart.addItem(myPizza);
+  //s9c
+  console.log(cart);
+  //s9d
+  let parForSummary = document.createElement("p");
+  parForSummary.textContent = myPizza.summarize();
+  cartBox.appendChild(parForSummary);
+  //s9e Resetting the page
+  clearPizzaImage();
+  clearToppings();
+}
