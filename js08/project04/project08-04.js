8298058eca78863e500c6c5ed6b3554a6b04cd87
fminor5 <fminor5@gmail.com>
2023-12-30 09:58:38 -0600

    Adding completed codding challenge 4.

--- js08/project04/project08-04.js
+++ js08/project04/project08-04.js
@@ -20,10 +20,48 @@ getFileButton.onchange = function () {
   fr.readAsText(JSONfile);
 
   // Once the file has finished loading, parse the JSON file
-  fr.onload = function () {};
+  fr.onload = function () {
+    //s3a Converting JSON data
+    let staff = JSON.parse(fr.result);
+    //s3b Calling the makeStaffTable
+    makeStaffTable(staff);
+  };
 };
 
 function makeStaffTable(staff) {
   let staffTable = document.createElement("table");
   let headerRow = document.createElement("tr");
+
+  //s5 using a for-in to iterate the object
+  for (let prop in staff.directory[0]) {
+    //s5a Creating a th element
+    let headerCell = document.createElement("th");
+    //s5b Storing the prop in headerCell *******Check this one
+    headerCell.textContent = prop;
+    //s5c appending the headerCell to header row
+    headerRow.appendChild(headerCell);
+  }
+
+  //s5d Appending header row to staffTable
+  staffTable.append(headerRow);
+
+  //s6 Using a for-loop to iterate through the items of staff.directory
+  for (let i = 0; i < staff.directory.length; i++) {
+    //s6a Creating a tr element
+    let tableRow = document.createElement("tr");
+    //s6b Using a for-in loop for each property on the staff.directory
+    for (let prop in staff.directory[i]) {
+      //s6b1 Creating an element td
+      let tableCell = document.createElement("td");
+      //s6b2  Storing the value of staff.directory
+      tableCell.textContent = staff.directory[i][prop];
+      //s6b3 Appending tableCell to tableRow
+      tableRow.append(tableCell);
+    }
+    //s6c Appeding tableRow to staffTable
+    staffTable.append(tableRow);
+  }
+
+  //s7 Appending staffTable to containerBox
+  containerBox.appendChild(staffTable);
 }
