17777f10422dac7e7513eb3667271783c570fa09
fminor5 <fminor5@gmail.com>
2024-01-04 12:06:34 -0600

    Viewing and retrieving items in files js09b and js09c.

--- js09/chapter/js09b.js
+++ js09/chapter/js09b.js
@@ -7,3 +7,52 @@
       
       Filename: js09b.js
  */
+
+// Retrieve the text of the query string
+let qString = location.search.slice(1);
+// console.log(qString);
+
+// Replace the encoded characters in the query string
+qString = qString.replace(/\+/g, " ");
+qString = decodeURIComponent(qString);
+
+// console.log(qString);
+
+// Split the field=name pairs into separate array items
+let formData = qString.split(/&/g);
+// console.log(formData);
+
+for (let items of formData) {
+  // Extract the field names and values
+  let fieldValuePair = items.split(/=/);
+  let fieldName = fieldValuePair[0];
+  let fieldValue = fieldValuePair[1];
+
+  //   Create a label containing the field name
+  let fieldLabel = document.createElement("label");
+  fieldLabel.textContent = fieldName;
+  document.getElementById("contactInfo").appendChild(fieldLabel);
+
+  // Create an disabled input box with the field value
+  let inputBox = document.createElement("input");
+  inputBox.id = fieldName;
+  inputBox.name = fieldName;
+  inputBox.value = fieldValue;
+  inputBox.disabled = true;
+  document.getElementById("contactInfo").appendChild(inputBox);
+}
+
+// Store data to local storage when the user signs up
+document.getElementById("signupBtn").onclick = function () {
+  // data fields to be saved to local storage
+  let formFields = document.querySelectorAll(
+    "#contactInfo input, input[type=radio], textarea"
+  );
+
+  // write each filed name and value to local storage
+  for (let fields of formFields) {
+    localStorage.setItem(fields.name, fields.value);
+  }
+
+  console.log(localStorage);
+};
--- js09/chapter/js09c.js
+++ js09/chapter/js09c.js
@@ -7,3 +7,34 @@
       
       Filename: js09c.js
  */
+// Eating Well Preference Keys
+let keys = [
+  "name",
+  "email",
+  "phone",
+  "address",
+  "city",
+  "state",
+  "zip",
+  "allergies",
+  "frequency",
+  "size",
+];
+
+// Retrieving values from local storage
+for (let item of keys) {
+  let newRow = document.createElement("tr");
+
+  // Display the storage key
+  let keyCell = document.createElement("td");
+  keyCell.textContent = item;
+  newRow.appendChild(keyCell);
+
+  // Display the key value
+  let keyValue = document.createElement("td");
+  keyValue.textContent = localStorage.getItem(item);
+  newRow.appendChild(keyValue);
+
+  // Append each key=name pair as a table row
+  document.getElementById("prefTable").appendChild(newRow);
+}
