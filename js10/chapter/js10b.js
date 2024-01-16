fa337d6cc268aabc4a7ea2fb3de8656e6c0af062
fminor5 <fminor5@gmail.com>
2024-01-16 09:49:35 -0600

    Leaflet map added to files.

--- js10/chapter/js10b.js
+++ js10/chapter/js10b.js
@@ -14,3 +14,15 @@ function initMap() {
   let displayMap = document.getElementById("displayMap");
   let routeBox = document.getElementById("routeBox");
 }
+// Set up and display the Oak Top House Map
+let map = L.map(displayMap).setView([39.96118, -82.99879], 13);
+
+L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([39.96118, -82.99879])
+  .addTo(map)
+  .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+  .openPopup();
