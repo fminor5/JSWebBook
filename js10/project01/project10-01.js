ddceddf3964e2f8d5d8b9959a7a6729925a2835e
fminor5 <fminor5@gmail.com>
2024-01-16 10:50:22 -0600

    Adding completed files for project 10-01.

--- js10/project01/project10-01.js
+++ js10/project01/project10-01.js
@@ -40,3 +40,43 @@ for (let i = 0; i < 48; i++) {
 
 // Node list representing the puzzle pieces
 let pieces = document.querySelectorAll("div#puzzleBoard img");
+
+//s3 Using a for-loop to iterate on the pieces node list.
+for (let i = 0; i < pieces.length; i++) {
+  pieces[i].addEventListener("pointerdown", grabPiece);
+}
+
+//s4 Creating the grabPiece function
+function grabPiece(e) {
+  //s4a Setting the values of pointers to clients.
+  pointerX = e.clientX;
+  pointerY = e.clientY;
+  //s4b Setting the value of touchAction to none
+  e.target.style.touchAction = "none";
+  //s4c Increasing the zCounter value by one and applying this to zIndex.
+  zCounter++;
+  e.target.style.zIndex = zCounter;
+  //s4d Setting the values of piece X and Y to offset Left and Top
+  pieceX = e.target.offsetLeft;
+  pieceY = e.target.offsetTop;
+  //s4e Adding event listeners to the event targets
+  e.target.addEventListener("pointermove", movePiece);
+  e.target.addEventListener("pointerup", dropPiece);
+}
+
+//s5 Creating the movePiece function
+function movePiece(e) {
+  //s5a Setting the diff variables
+  let diffX = e.clientX - pointerX;
+  let diffY = e.clientY - pointerY;
+  //s5b
+  e.target.style.left = pieceX + diffX + "px";
+  e.target.style.top = pieceY + diffY + "px";
+}
+
+//s6 Creating the dropPiece function
+function dropPiece(e) {
+  //s6a
+  e.target.removeEventListener("pointermove", movePiece);
+  e.target.removeEventListener("pointerup", dropPiece);
+}
