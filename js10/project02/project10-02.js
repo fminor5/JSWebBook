70dbb25486abb5ff19a7592f03c18622c2544d68
fminor5 <fminor5@gmail.com>
2024-01-17 10:44:42 -0600

    Adding files for project 10-02 but it is not working properly.

--- js10/project02/project10-02.js
+++ js10/project02/project10-02.js
@@ -37,3 +37,51 @@ function rotateTan(elem, deg) {
 
   elem.style.transform = "rotate(" + newAngle + "deg)";
 }
+
+//s3 Using a for-loop to iterate through all the items in the tans list.
+for (let i = 0; i < tans.length; i++) {
+  tans[i].addEventListener("pointerdown", grabTan);
+}
+
+//s4 Creating the grabTan function
+function grabTan(e) {
+  //s4a If shift key is pressed call the rotateTan fucntion
+  if (e.shiftKey) {
+    rotateTan(e.target, 15);
+  } else {
+    //s4b Storing the values of clients X and Y into the events X and Y
+    //Storing the pointer's initial position
+    eventX = e.clientX;
+    eventY = e.clientY;
+    //s4b
+    e.target.style.touchAction = "none";
+    //s4b increasing the value of zCounter by 1
+    zCounter++;
+    //s4b applying the zIndex to the style e target
+    e.target.style.zIndex = zCounter;
+
+    //No in the instructions
+    //Storing the piece inital position
+    eventX = e.target.offsetLeft;
+    eventY = e.target.offsetTop;
+    //s4c
+    e.target.addEventListener("pointermove", moveTan);
+    e.target.addEventListener("pointerup", dropTan);
+  }
+}
+
+//s5 Creating the moveTan function
+function moveTan(e) {
+  //Calculate the distance the pointer travelled horizontally and vertically
+  let diffX = e.clientX - eventX;
+  let diffY = e.clientY - eventY;
+  //Move the piece the same distance as the pointer.
+  e.target.style.left = eventX + diffX + "px";
+  e.target.style.top = eventY + diffY + "px";
+}
+
+//s6 Creating the dropTan function
+function dropTan(e) {
+  e.target.removeEventListener("pointermove", moveTan);
+  e.target.removeEventListener("pointerup", dropTan);
+}
