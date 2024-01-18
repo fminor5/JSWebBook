f3ae0c00bafccb90332e6acbb18c94b4e4679f28
fminor5 <fminor5@gmail.com>
2024-01-18 11:46:43 -0600

    Woring on js file. Dropping object function is still not working.

--- js10/project04/project10-4.js
+++ js10/project04/project10-4.js
@@ -13,3 +13,47 @@ let pieces = document.getElementsByTagName("span");
 let boardSquares = document.querySelectorAll("table#chessboard td");
 let whiteBox = document.getElementById("whiteBox");
 let blackBox = document.getElementById("blackBox");
+
+//s3 Using a loop to iterate through all the contents of pieces.
+for (let piece of pieces) {
+  //s3a Setting the value of piece draggable to true.
+  piece.draggable = true;
+  //s3b
+  piece.ondragstart = function (e) {
+    e.dataTransfer.setData("text", piece.id);
+  };
+}
+
+//s4 Using a loop to iterate through all the items of the boardSquares
+for (let bsquare of boardSquares) {
+  //s5
+  bsquare.ondragover = function (e) {
+    e.preventDefault();
+  };
+  //s6
+  bsquare.ondrop = function (e) {
+    //s7
+    e.preventDefault();
+    //s8
+    let pieceID = e.dataTransfer.getData("text");
+    //s9
+    let movingPiece = document.getElementById(pieceID);
+    //s10
+    if (e.target.tagName === "TD") {
+      e.target.appendChild(movingPiece);
+    } else if (e.target.tagName === "SPAN") {
+      //s11a
+      let occupyingPiece = e.target;
+      //s11b
+      let square = occupyingPiece.parentElement;
+      //s11c
+      square.appendChild(movingPiece);
+      //s11d
+      if (occupyingPiece.className === "white") {
+        whiteBox.appendChild(occupyingPiece);
+      } else {
+        blackBox.appendChild(occupyingPiece);
+      }
+    }
+  };
+}
