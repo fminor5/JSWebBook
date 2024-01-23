a4f3437e1db29e0bed778c60d25359724d76d616
fminor5 <fminor5@gmail.com>
2024-01-23 09:15:24 -0600

    Adding files for chapter 11

---
+++ js11/project01/project11-01.js
@@ -0,0 +1,14 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 11
+      Project 11-01
+
+      Project to retrieve the Astronomy Picture of the Day from NASA
+
+      Filename: project11-01.js
+*/
+
+let imageBox = document.getElementById("nasaImage");
+let dateBox = document.getElementById("dateBox");
+
+dateBox.onchange = function () {};
---
+++ js11/project02/project11-02.js
@@ -0,0 +1,16 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 11
+      Project 11-02
+
+      Project to city and state information from a provided postal code
+
+      Filename: project11-02.js
+*/
+
+let postalCode = document.getElementById("postalCode");
+let place = document.getElementById("place");
+let region = document.getElementById("region");
+let country = document.getElementById("country");
+
+postalCode.onblur = function () {};
