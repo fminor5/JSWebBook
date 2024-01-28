19e264ea045d0870146d13e757434c24be225d4e
fminor5 <fminor5@gmail.com>
2024-01-28 09:53:40 -0600

    Adding completed files for project 2.

--- js11/project02/project11-02.js
+++ js11/project02/project11-02.js
@@ -13,4 +13,23 @@ let place = document.getElementById("place");
 let region = document.getElementById("region");
 let country = document.getElementById("country");
 
-postalCode.onblur = function () {};
+postalCode.onblur = function () {
+  //3a
+  let codeValue = postalCode.value;
+  let countryValue = country.value;
+  //3b
+  place.value = "";
+  region.value = "";
+  //4c
+  fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+    //4d
+    .then((response) => response.json())
+    //4e
+    .then((json) => {
+      place.value = json.places[0]["place name"];
+      region.value = json.places[0]["state abbreviation"];
+      console.log(json);
+    })
+    //4f
+    .catch((error) => console.log(error));
+};
