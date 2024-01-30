f94f83dc4d42c3c46559d792807a3c12b8601aa3
fminor5 <fminor5@gmail.com>
2024-01-30 10:55:59 -0600

    Adding completed files for project 11-4

--- js11/project04/project11-04.js
+++ js11/project04/project11-04.js
@@ -28,6 +28,42 @@ function getLocation(pos) {
     lat: pos.coords.latitude,
     lng: pos.coords.longitude,
   };
+  console.log(myPosition.lat);
+  console.log(myPosition.lng);
+  //s5
+  let url = "https://api.openuv.io/api/v1/uv";
+  let key = "yourKey";
+  //s6
+  fetch(`${url}?lat=${myPosition.lat}&lng=${myPosition.lng}`, {
+    //s7a
+    method: "GET",
+    //s7b
+    headers: { "x-access-token": key },
+  })
+    //s8
+    .then((response) => response.json())
+    //s9
+    .then((json) => showSunSafety(json, myPosition))
+    //s10
+    .catch((error) => console.log(error));
+}
+
+function showSunSafety(json, myPosition) {
+  console.log(myPosition);
+  //s11a
+  latCell.textContent = myPosition.lat;
+  lngCell.textContent = myPosition.lng;
+  //s11b
+  uvIndexCell.textContent = json.result.uv;
+  uvMaxCell.textContent = json.result.uv_max;
+  ozoneCell.textContent = json.result.ozone;
+  //s11c
+  st1Cell.textContent = json.result.safe_exposure_time.st1;
+  st2Cell.textContent = json.result.safe_exposure_time.st2;
+  st3Cell.textContent = json.result.safe_exposure_time.st3;
+  st4Cell.textContent = json.result.safe_exposure_time.st4;
+  st5Cell.textContent = json.result.safe_exposure_time.st5;
+  st6Cell.textContent = json.result.safe_exposure_time.st6;
 }
 
 function handleError() {
