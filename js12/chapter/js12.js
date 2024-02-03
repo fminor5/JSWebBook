e38df925103b19cccdfdd642c1e8bacac8d60242
fminor5 <fminor5@gmail.com>
2024-02-03 11:17:09 -0600

    Adding sections, working with jQuery selectors, and handling events with jQuery.

--- js12/chapter/js12.js
+++ js12/chapter/js12.js
@@ -8,3 +8,20 @@
 
     Filename: js12.js
 */
+
+// Run once the page is loaded and ready
+$(() => {
+  // Add click events to each question in the FAQ
+  $("dl#faq dt").click((e) => {
+    // Alternate between hiding and showing the answer
+    let question = $(e.target);
+    let answer = $(question.next());
+    $(question).toggleClass("hiddenAnswer");
+
+    if ($(question).hasClass("hiddenAnswer")) {
+      $(answer).hide();
+    } else {
+      $(answer).show();
+    }
+  });
+});
