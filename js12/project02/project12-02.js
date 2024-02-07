6f478082716e12ffadf52599c3655a834d38f837
fminor5 <fminor5@gmail.com>
2024-02-07 10:25:23 -0600

    Adding completed files for project 12-2

--- js12/project02/project12-02.js
+++ js12/project02/project12-02.js
@@ -7,3 +7,25 @@
 
       Filename: project12-02.js
 */
+
+$(() => {
+  // s3
+  $("input#cValue").change((e) => {
+    // s3a
+    let celsius = $(e.target).val();
+    // s3b
+    let fahrenheit = 1.8 * celsius + 32;
+    // s3c
+    $("input#fValue").val(fahrenheit.toFixed(0));
+  });
+
+  // s4
+  $("input#fValue").change((e) => {
+    // s4a
+    let fahrenheit = $(e.target).val();
+    // s4b
+    let celsius = (fahrenheit - 32) / 1.8;
+    // s4c
+    $("input#cValue").val(celsius.toFixed(0));
+  });
+});
