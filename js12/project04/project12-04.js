40b2f5e1c96ef37d4e6d3d6fb5043fb9f89bb12e
fminor5 <fminor5@gmail.com>
2024-02-08 11:08:46 -0600

    Adding completed step but they have not been tested yet

--- js12/project04/project12-04.js
+++ js12/project04/project12-04.js
@@ -7,3 +7,74 @@
 
       Filename: project12-04.js
 */
+
+//s3
+let slideNumber = 0;
+
+$(() => {
+  //s4
+  $("img#leftbutton").click((e) => {
+    //s5
+    if (slideNumber > 0) {
+      //s5a
+      $("img.slideImages").animate(
+        {
+          left: "+=401px",
+        },
+        1000
+      );
+      //s5b
+      slideImage -= 1;
+      //s5c
+      let currentSlide = $("img.slidesImages");
+      //s5d
+      let slideCaption = $(currentSlide).attr("alt");
+      //s5e
+      changeCaption(slideCaption);
+    }
+  });
+});
+
+//s6
+$(() => {
+  $("img#rightbutton").click((e) => {
+    //s6b
+    if (slideNumber < 11) {
+      $("img.slideImages").animate(
+        {
+          //s6c
+          left: "-=401px",
+        },
+        1000
+      );
+      //s6d
+      slideImage += 1;
+      let currentSlide = $("img.slidesImages");
+      let slideCaption = $(currentSlide).attr("alt");
+      changeCaption(slideCaption);
+    }
+  });
+});
+
+//s7
+function changeCaption(captionText) {
+  $("div#caption")
+    .effect(
+      "blind",
+      {
+        mode: "hide",
+        direction: "left",
+      },
+      500,
+      () => {
+        $("div#caption").text(captionText);
+      }
+    )
+    .effect(
+      "show",
+      {
+        direction: "left",
+      },
+      500
+    );
+}
