6bf5a6b3d216180cf59c6b6e837f71f5ab62a7d1
fminor5 <fminor5@gmail.com>
2024-02-08 10:20:08 -0600

    Adding completed files for project 12-3

--- js12/project03/project12-03.js
+++ js12/project03/project12-03.js
@@ -7,3 +7,24 @@
 
       Filename: project12-03.js
 */
+
+// s3
+$(() => {
+  // s3
+  $("article > h2").click((e) => {
+    //s4a
+    let heading = $(e.target);
+    //s4b
+    let list = $(heading).next();
+    //s4c
+    let headingImage = $(heading).children("img");
+    //s5
+    $(list).slideToggle(500);
+    //s6
+    if ($(headingImage).attr("src") === "plus.png") {
+      $(headingImage).attr("src", "minus.png");
+    } else {
+      $(headingImage).attr("src", "plus.png");
+    }
+  });
+});
